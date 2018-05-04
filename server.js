b491eb85fb4d1b7590a847a7a7aa8f10bb6e8f1d
Connor <wscofield@elon.edu>
2018-05-03 20:12:54 -0400

    basic html completed, beginning work on scraping data

---
+++ server.js
@@ -0,0 +1,9 @@
+// See near the bottom of this file for your TODO assignments.
+// Good luck!
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+var cheerio = require("cheerio");
+var request = require("request");
+
