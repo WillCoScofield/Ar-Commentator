99ee3fe386d0c1f7fc13228318ce2c42c17f575b
Connor <wscofield@elon.edu>
2018-05-07 12:36:40 -0400

    Missing CSS styling somehow, copied from github, hopefully back to where I was prior to last week

--- server.js
+++ server.js
@@ -6,4 +6,21 @@ var express = require("express");
 var mongojs = require("mongojs");
 var cheerio = require("cheerio");
 var request = require("request");
+var axios = require("axios");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
+
+
+
+
+
+console.log("===SCRAPING NPR TECH=====")
+
+var url = "https://www.npr.org/sections/alltechconsidered/195149875/innovation";
+
+
+
+
+
+console.log(data);
